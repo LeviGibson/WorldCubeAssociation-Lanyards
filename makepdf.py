268c3a7ed83c087d1391534090d5e350f11a5902
LeviGibson <72767501+LeviGibson@users.noreply.github.com>
2022-09-23 09:34:42 -0400

    centralized pages

--- makepdf.py
+++ makepdf.py
@@ -3,9 +3,27 @@ from PIL import Image
 import glob
 import subprocess
 
-TOP_EDGE_PADDING = 60
-SIDE_EDGE_PADDING = 400
-PADDING_BETWEEN_CARDS = 10
+PADDING_BETWEEN_CARDS = 20
+PAGEWIDTH = int(8.5*500)
+PAGEHEIGHT = int(11*500)
+
+CARDWIDTH = 3370//2
+CARDHEIGHT = 2125//2
+
+def get_side_padding():
+    fullsize = (CARDWIDTH*2) + (PADDING_BETWEEN_CARDS*1)
+    padding = (PAGEWIDTH - fullsize)//2
+
+    return padding
+
+def get_top_padding():
+    fullsize = CARDHEIGHT*5 + (PADDING_BETWEEN_CARDS*4)
+    padding = (PAGEHEIGHT - fullsize)//2
+
+    return padding
+
+TOP_EDGE_PADDING = get_top_padding()
+SIDE_EDGE_PADDING = get_side_padding()
 
 subprocess.call("rm -f pngs/*.png", shell=True)
 
@@ -17,26 +35,21 @@ cardsDone = 0
 canvasesDone = 0
 
 
-def get_card_image(cardsDone):
-    card = Image.open(cards[cardsDone])
-    ratio = min((3370/2)/card.size[0], (2125/2)/card.size[1])
-    card = card.resize((int(card.size[0]*ratio), int(card.size[1]*ratio)))
-    return card
-
 def run():
     global cardsDone, canvasesDone
 
     while cardsDone < len(inserts):
-        cardcanvas = Image.new('RGB', (int(8.5*500), 11*500), (255, 255, 255))
-        insertcanvas = Image.new('RGB', (int(8.5*500), 11*500), (255, 255, 255))
+        cardcanvas = Image.new('RGB', (PAGEWIDTH, PAGEHEIGHT), (255, 255, 255))
+        insertcanvas = Image.new('RGB', (PAGEWIDTH, PAGEHEIGHT), (255, 255, 255))
 
         for x in range(2):
             for y in range(5):
                 card = Image.open(inserts[cardsDone])
-                card = card.resize((3370//2, 2125//2))
+                card = card.resize((CARDWIDTH, CARDHEIGHT))
                 insertcanvas.paste(card, (x*(3370//2 + PADDING_BETWEEN_CARDS) + SIDE_EDGE_PADDING, y*(2125//2 + PADDING_BETWEEN_CARDS) + TOP_EDGE_PADDING))
 
-                card = get_card_image(cardsDone)
+                card = Image.open(cards[cardsDone])
+                card = card.resize((CARDWIDTH, CARDHEIGHT))
                 cardcanvas.paste(card, ((1-x)*(3370//2 + PADDING_BETWEEN_CARDS) + SIDE_EDGE_PADDING, y*(2125//2 + PADDING_BETWEEN_CARDS) + TOP_EDGE_PADDING))
 
                 cardsDone+=1
